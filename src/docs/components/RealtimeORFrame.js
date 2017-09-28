c0ca757257bbfbda9efa914a67b8f48bf9ee25ab
Elijah Meeks <elijahmeeks@gmail.com>
2017-09-28 11:59:32 -0700

    Add source and explanation of realtime examples

--- src/docs/components/RealtimeORFrame.js
+++ src/docs/components/RealtimeORFrame.js
@@ -12,8 +12,6 @@ components.push({
 
 let columnNumber = 1;
 
-const eventTypes = ["warning", "error", "information"];
-
 const eventColors = {
   warning: "#4d430c",
   error: "#b3331d",
@@ -187,6 +185,179 @@ export default class RealtimeORFrame extends React.Component {
         />
       ),
       source: `
+let fakeRealtimeDataInterval;
+
+let columnNumber = 1;
+
+const eventColors = {
+  warning: "#4d430c",
+  error: "#b3331d",
+  information: "#b6a756"
+};
+const dataStart = new Date();
+
+const barData = [
+  {
+    time: dataStart.getTime(),
+    eventType: "information",
+    number: 2,
+    column: "column-1"
+  },
+  {
+    time: dataStart.getTime(),
+    eventType: "warning",
+    number: 5,
+    column: "column-1"
+  },
+  {
+    time: dataStart.getTime(),
+    eventType: "error",
+    number: 1,
+    column: "column-1"
+  }
+];
+const orAnnotation = {
+  type: "or",
+  time: dataStart.getTime(),
+  number: 2,
+  dy: -20,
+  dx: 0,
+  id: "semiotic",
+  label: "awesome?"
+};
+
+export default class RealtimeORFrame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: barData,
+      calloutAnnotations: [
+        orAnnotation,
+        {
+          type: "enclose",
+          rp: "top",
+          rd: 25,
+          coordinates: [
+            { time: dataStart.getTime(), number: 7, column: "column-1" },
+            { time: dataStart.getTime(), number: 8, column: "column-1" }
+          ],
+          label: "Error"
+        }
+      ]
+    };
+  }
+  componentWillUnmount() {
+    clearInterval(fakeRealtimeDataInterval);
+  }
+
+  componentDidMount() {
+    fakeRealtimeDataInterval = setInterval(() => {
+      const newData = this.state.data;
+      const now = new Date();
+      columnNumber += 1;
+      if (now.getTime() - dataStart.getTime() > 60000) {
+        clearInterval(fakeRealtimeDataInterval);
+      }
+      const errorAmount = parseInt(Math.random() * 3);
+      const infoAmount = parseInt(Math.random() * 8);
+      const warningAmount = parseInt(Math.random() * 5);
+      const totalEvents = errorAmount + infoAmount + warningAmount;
+
+      newData.push({
+        time: now.getTime(),
+        eventType: "information",
+        number: infoAmount,
+        column: ${"`column-${columnNumber}`"}
+      });
+      newData.push({
+        time: now.getTime(),
+        eventType: "warning",
+        number: warningAmount,
+        column: ${"`column-${columnNumber}`"}
+      });
+      newData.push({
+        time: now.getTime(),
+        eventType: "error",
+        number: errorAmount,
+        column: ${"`column-${columnNumber}`"}
+      });
+      if (newData.length > 60) {
+        newData.splice(0, 3);
+      }
+      orAnnotation.time = now.getTime();
+      orAnnotation.column = ${"`column-${columnNumber}`"}
+      orAnnotation.number = totalEvents - errorAmount;
+      orAnnotation.label = ${"`Non-Errrors: ${totalEvents - errorAmount}`"};
+      orAnnotation.label = (
+        <tspan
+          dy={12}
+          style={{
+            fontWeight: 900,
+            fill: "black",
+            stroke: "darkred",
+            strokeWidth: 0.5
+          }}
+        >{${"`Non-Errrors: ${totalEvents - errorAmount}`"}}</tspan>
+      );
+      this.setState({
+        data: newData,
+        calloutAnnotations: [
+          orAnnotation,
+          {
+            type: "enclose",
+            rp: "top",
+            rd: 25,
+            coordinates: [
+              {
+                time: now.getTime(),
+                number: totalEvents - errorAmount,
+                column: ${"`column-${columnNumber}`"}
+              },
+              {
+                time: now.getTime(),
+                number: totalEvents,
+                column: ${"`column-${columnNumber}`"}
+              }
+            ],
+            label: ${"`Errors: ${errorAmount}`"}
+          }
+        ]
+      });
+    }, 1000);
+  }
+
+  render() {
+        <ORFrame
+          size={[700, 300]}
+          data={barData}
+          oAccessor={"column"}
+          rAccessor={"number"}
+          type={barData.length > 28 ? "point" : "bar"}
+          style={d => ({
+            fill: eventColors[d.eventType],
+            stroke: eventColors[d.eventType]
+          })}
+          renderKey={d => d.time + d.eventType}
+          axis={{ orient: "left" }}
+          margin={{ top: 50, bottom: 0, left: 50, right: 50 }}
+          connectorStyle={d => ({
+            fill: eventColors[d.source.eventType],
+            stroke: eventColors[d.source.eventType]
+          })}
+          connectorType={d => d.eventType}
+          annotations={barData.length > 28 ? [] : this.state.calloutAnnotations}
+          oPadding={10}
+          hoverAnnotation={true}
+          tooltipContent={({ pieces }) => (
+            <div className="tooltip-content">
+              <p>Errors: {pieces[2].number}</p>
+              <p>Warnings: {pieces[1].number}</p>
+              <p>Information: {pieces[0].number}</p>
+            </div>
+          )}
+        />
+    }
+  }
       `
     });
 
@@ -197,7 +368,20 @@ export default class RealtimeORFrame extends React.Component {
         examples={examples}
         buttons={[]}
       >
-        <p />
+        <p>
+          A simple example of how to serve streaming data into an ORFrame. This
+          creates a stream using a simple setInterval script. The settings are
+          the same as you would see elsewhere (except with some behavior to
+          filter the data and update annotations) but the important thing to
+          notice is the use of{" "}
+          <span style={{ fontWeight: 900 }}>renderKey</span>. When you set
+          renderKey you're telling the frame how to identify a data
+          visualization element--similar to the optional key function in D3's
+          data(). Without setting renderKey, elements are considered to be the
+          same based on array position. When you specify a renderKey this
+          ensures that animated elements don't change when the amount of
+          elements being sent as data to the frame changes.
+        </p>
       </DocumentComponent>
     );
   }
--- src/docs/components/RealtimeXYFrame.js
+++ src/docs/components/RealtimeXYFrame.js
@@ -5,15 +5,13 @@ import { curveCardinal } from "d3-shape";
 
 const components = [];
 
-let fakeRealtimeDataInterval;
-
 components.push({
   name: "Realtime Line CHart"
 });
 
-let columnNumber = 1;
+let fakeRealtimeDataInterval;
 
-const eventTypes = ["warning", "error", "information"];
+let columnNumber = 1;
 
 const dataStart = new Date();
 const eventLines = {
@@ -155,6 +153,147 @@ export default class RealtimeXYFrame extends React.Component {
         />
       ),
       source: `
+let fakeRealtimeDataInterval;
+
+let columnNumber = 1;
+
+const dataStart = new Date();
+const eventLines = {
+  information: {
+    lineID: "Information",
+    color: "#b6a756",
+    coordinates: [
+      {
+        time: dataStart.getTime(),
+        number: 5
+      }
+    ]
+  },
+  error: {
+    lineID: "Error",
+    color: "#b3331d",
+    coordinates: [
+      {
+        time: dataStart.getTime(),
+        number: 1
+      }
+    ]
+  },
+  warning: {
+    lineID: "Warning",
+    color: "#4d430c",
+    coordinates: [
+      {
+        time: dataStart.getTime(),
+        number: 2
+      }
+    ]
+  }
+};
+const eventData = [
+  eventLines.error,
+  eventLines.warning,
+  eventLines.information
+];
+
+const xyAnnotation = {
+  type: "xy",
+  time: dataStart.getTime(),
+  dy: 5,
+  dx: 0,
+  lineID: "Error"
+};
+
+export default class RealtimeXYFrame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: eventData,
+      calloutAnnotations: [xyAnnotation]
+    };
+  }
+
+  componentWillUnmount() {
+    clearInterval(fakeRealtimeDataInterval);
+  }
+
+  componentDidMount() {
+    fakeRealtimeDataInterval = setInterval(() => {
+      const newData = this.state.data;
+      const now = new Date();
+      columnNumber += 1;
+      if (now.getTime() - dataStart.getTime() > 60000) {
+        clearInterval(fakeRealtimeDataInterval);
+      }
+      const errorAmount = parseInt(Math.random() * 3);
+      const infoAmount = parseInt(Math.random() * 8);
+      const warningAmount = parseInt(Math.random() * 5);
+      const totalEvents = errorAmount + infoAmount + warningAmount;
+
+      eventLines.error.coordinates.push({
+        time: now.getTime(),
+        number: errorAmount
+      });
+      eventLines.warning.coordinates.push({
+        time: now.getTime(),
+        number: warningAmount
+      });
+      eventLines.information.coordinates.push({
+        time: now.getTime(),
+        number: infoAmount
+      });
+
+      xyAnnotation.column = ${"`column-${columnNumber}`"};
+      this.setState({
+        data: newData,
+        calloutAnnotations: [xyAnnotation]
+      });
+      xyAnnotation.time = now.getTime();
+      xyAnnotation.label = (
+        <tspan
+          dy={-12}
+          style={{
+            fontWeight: 900,
+            fill: "black",
+            stroke: "black",
+            strokeWidth: 0.5
+          }}
+        >{${"`ERRORS: ${errorAmount}`}</tspan>"}
+      );
+    }, 1000);
+  }
+
+  render() {
+   <XYFrame
+          size={[700, 300]}
+          lines={eventData}
+          lineDataAccessor={d =>
+            d.coordinates.filter((p, q) => q > d.coordinates.length - 10)}
+          xAccessor={"time"}
+          yAccessor={"number"}
+          lineType={{
+            type:
+              eventData[0].coordinates.length < 10
+                ? "line"
+                : eventData[0].coordinates.length < 20
+                  ? "stackedarea"
+                  : "bumparea",
+            interpolator: curveCardinal
+          }}
+          lineIDAccessor="lineID"
+          lineStyle={d => ({
+            fill: d.color,
+            stroke: d.color
+          })}
+          renderKey={d => d.lineID}
+          axes={[{ orient: "left" }]}
+          margin={{ top: 50, bottom: 0, left: 50, right: 50 }}
+          annotations={this.state.calloutAnnotations}
+          hoverAnnotation={true}
+        />
+    }
+  }
+
       `
     });
 
@@ -165,7 +304,24 @@ export default class RealtimeXYFrame extends React.Component {
         examples={examples}
         buttons={[]}
       >
-        <p />
+        <p>
+          A simple example of how to serve streaming data into an XYFrame. This
+          creates a stream using a simple setInterval script. The settings are
+          the same as you would see elsewhere (except with some behavior to
+          filter the data and update annotations) but the important thing to
+          notice is the use of{" "}
+          <span style={{ fontWeight: 900 }}>renderKey</span>. When you set
+          renderKey you're telling the frame how to identify a data
+          visualization element--similar to the optional key function in D3's
+          data(). Without setting renderKey, elements are considered to be the
+          same based on array position. When you specify a renderKey this
+          ensures that animated elements don't change when the amount of
+          elements being sent as data to the frame changes.
+        </p>
+        <p>
+          This example demonstrates why there needs to be a nice solution to
+          interpolation of a growing line/area.
+        </p>
       </DocumentComponent>
     );
   }
