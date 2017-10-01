85fb34321b9baf3501bbf29b80d41ecd6c9c6bed
Elijah Meeks <elijahmeeks@gmail.com>
2017-09-30 23:35:04 -0700

    Responsive Frames use react-resize-detector
    Pull out `react-dimensions` and replace with `react-resize-detector` for ResponsiveFrames

--- src/components/ResponsiveFrame.js
+++ src/components/ResponsiveFrame.js
@@ -1,45 +1,69 @@
 import React from "react";
-import Dimensions from "react-dimensions";
+import ReactResizeDetector from "react-resize-detector";
 import XYFrame from "./XYFrame";
 import ORFrame from "./ORFrame";
 import NetworkFrame from "./NetworkFrame";
 import SmartFrame from "./SmartFrame";
 import MinimapXYFrame from "./MinimapXYFrame";
 
-const createResponsiveFrame = Frame => {
-  return Dimensions()(
-    class ResponsiveFrame extends React.Component {
-      render() {
-        const {
-          responsiveWidth,
-          responsiveHeight,
-          size,
-          containerHeight,
-          containerWidth,
-          dataVersion
-        } = this.props;
+const createResponsiveFrame = Frame =>
+  class ResponsiveFrame extends React.Component {
+    constructor(props) {
+      super(props);
 
-        if (responsiveWidth) {
-          size[0] = containerWidth;
-        }
+      this._onResize = this._onResize.bind(this);
 
-        if (responsiveHeight) {
-          size[1] = containerHeight;
-        }
+      this.state = {
+        containerHeight: props.size[1] || 500,
+        containerWidth: props.size[0] || 500
+      };
 
-        let dataVersionWithSize = dataVersion + size.toString();
+      this.oAccessor = null;
+      this.rAccessor = null;
+      this.oScale = null;
+      this.rScale = null;
+    }
 
-        return (
+    _onResize(width, height) {
+      this.setState({ containerHeight: height, containerWidth: width });
+    }
+
+    render() {
+      const {
+        responsiveWidth,
+        responsiveHeight,
+        size,
+        dataVersion
+      } = this.props;
+
+      const { containerHeight, containerWidth } = this.state;
+
+      if (responsiveWidth) {
+        size[0] = containerWidth;
+      }
+
+      if (responsiveHeight) {
+        size[1] = containerHeight;
+      }
+
+      let dataVersionWithSize = dataVersion + size.toString();
+
+      return (
+        <div className="responsive-container">
           <Frame
             {...this.props}
             size={size}
             dataVersion={dataVersion ? dataVersionWithSize : undefined}
           />
-        );
-      }
+          <ReactResizeDetector
+            handleWidth={responsiveWidth}
+            handleHeight={responsiveHeight}
+            onResize={this._onResize}
+          />
+        </div>
+      );
     }
-  );
-};
+  };
 
 export const ResponsiveXYFrame = createResponsiveFrame(XYFrame);
 export const ResponsiveORFrame = createResponsiveFrame(ORFrame);
