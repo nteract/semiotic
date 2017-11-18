a2c65f314aaf809a942bc0d71e9c4e024fa791ad
Tom MacWright <tom@macwright.org>
2017-11-18 14:44:43 -0800

    Configure rollup to work with process.env substitution, node modules, and more

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,30 @@
 import node from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
+import builtins from "rollup-plugin-node-builtins";
+import replace from "rollup-plugin-replace";
 
 export default {
   input: "src/components/index.js",
   output: {
     format: "umd",
-    file: 'dist/semioitic.js',
+    file: 'dist/semiotic.js',
     name: 'semiotic'
   },
+  interop: false,
   globals: {
     react: "React",
     "react-dom": "ReactDOM"
   },
   external: ["react", "react-dom"],
   plugins: [
-    node({jsnext: true}),
+    node({jsnext: true, preferBuiltins: false}),
+    builtins(),
     commonjs({
-      include: "node_modules/**",
-      ignore: [
-        "./cjs/react-dom.production.min.js",
-        "./cjs/react.production.min.js"
-      ]
+      include: "node_modules/**"
+    }),
+    replace({
+      'process.env.NODE_ENV': '"production"'
     }),
     babel({
       babelrc: false,
@@ -34,7 +37,6 @@ export default {
       ],
       'plugins': [
         'transform-decorators-legacy',
-        'add-module-exports',
         'transform-object-assign',
         'react-require',
         'external-helpers'
