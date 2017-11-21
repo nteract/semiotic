00b56e47d915d103a55ef41514cc29979621dedd
Elijah Meeks <elijahmeeks@gmail.com>
2017-11-20 17:20:43 -0800

    Finish minimap example

--- src/docs/components/Minimap.js
+++ src/docs/components/Minimap.js
@@ -42,7 +42,87 @@ export default class NegativeStacked extends React.Component {
         this.state.resetExtent,
         this.state.selectedExtent
       ),
-      source: `
+      source: `import { MinimapXYFrame } from "../../components";
+import { curveMonotoneX } from "d3-shape";
+
+const dataSeeds = [20, 10, -10, -20];
+const colors = [
+  "4d430c",
+  "#d38779",
+  "#b3331d",
+  "#00a2ce",
+  "#007190",
+  "#b6a756"
+];
+
+function generatePoints(start, number) {
+  const arrayOfPoints = [];
+  let currentValue = start;
+  for (let x = 0; x <= number; x++) {
+    arrayOfPoints.push({ step: x, value: currentValue });
+    currentValue += Math.random() * 10 - 5;
+  }
+  return arrayOfPoints;
+}
+
+const generatedData = dataSeeds.map((s, i) => {
+  return {
+    label: colors[i],
+    coordinates: generatePoints(s, 40)
+  };
+});
+
+const lineStyle = {
+  fill: "#007190",
+  stroke: "#007190",
+  strokeWidth: 1
+};
+
+const xyFrameSettings = {
+  lines: generatedData,
+  lineType: { type: "line", interpolator: curveMonotoneX },
+  xAccessor: "step",
+  yAccessor: "value",
+  lineStyle: d => ({ fill: d.label, stroke: d.label, fillOpacity: 0.75 }),
+  axes: [
+    { orient: "left" },
+    {
+      orient: "bottom",
+      ticks: 6
+    }
+  ]
+};
+//In your Component where you're creating the MinimapXYFrame
+  constructor(props) {
+    super(props);
+    this.state = { resetExtent: [0, 40], selectedExtent: [0, 40] };
+    this.randomizeExtent = this.randomizeExtent.bind(this);
+    this.changeExtent = this.changeExtent.bind(this);
+  }
+  randomizeExtent() {
+    const randomStart = parseInt(Math.random() * 25);
+    this.setState({ resetExtent: [randomStart, randomStart + 15] });
+  }
+
+  changeExtent(e) {
+    this.setState({ selectedExtent: [Math.floor(e[0]), Math.ceil(e[1])] });
+  }
+//
+<MinimapXYFrame
+    size={[700, 700]}
+    {...xyFrameSettings}
+    xExtent={selectedExtent}
+    matte={true}
+    margin={{ left: 50, top: 10, bottom: 50, right: 20 }}
+    minimap={{
+      margin: { top: 20, bottom: 35, left: 20, right: 20 },
+      ...xyFrameSettings,
+      brushEnd: brushFunction,
+      yBrushable: false,
+      xBrushExtent: extent,
+      size: [700, 150]
+    }}
+  />
   `
     });
 
@@ -53,7 +133,20 @@ export default class NegativeStacked extends React.Component {
         examples={examples}
         buttons={buttons}
       >
-        <p>MinimapXYFrame gives you some controls</p>
+        <p>
+          MinimapXYFrame allows you to conveniently instantiate a brushable
+          region, typically referred to as a minimap, to let users brush to zoom
+          in to a particular extent. Here's it's used to allow users to zoom to
+          a particular part of a line chart. The minimap property in
+          MinimapXYFrame takes an object with settings almost identical to
+          XYFrame except it also includes properties for brush behavior and
+          extent like brushEnd, yBrushable, xBrushable, xBrushExtent and the
+          functions for brush, brushstart and brushEnd.
+        </p>
+        <p>
+          You can programmatically change brush extent by sending a new
+          xBrushExtent.
+        </p>
       </DocumentComponent>
     );
   }
