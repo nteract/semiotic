458c6f5061332f5f345628a4fd49fa22f3def3da
Elijah Meeks <elijahmeeks@gmail.com>
2017-12-04 09:36:35 -0800

    Multi-subject annotations

--- src/components/Annotation.js
+++ src/components/Annotation.js
@@ -1,25 +1,43 @@
 import React from "react";
-import { AnnotationLabel } from "react-annotation";
+import { AnnotationLabel, Annotation, SubjectCircle } from "react-annotation";
 
 import PropTypes from "prop-types";
 
-class Annotation extends React.Component {
+class SemioticAnnotation extends React.Component {
   render() {
-    const noteData = this.props.noteData;
+    const { noteData } = this.props;
+    const { screenCoordinates } = noteData;
 
     noteData.type =
       typeof noteData.type === "function" ? noteData.type : AnnotationLabel;
 
-    //TODO come back and implement event listeners
+    const eventListeners = noteData.eventListeners || {};
 
-    const eventListeners = this.props.noteData.eventListeners || {};
-    // return <g {...eventListeners} ref={node => (this.node = node)} />;
+    if (noteData.coordinates && screenCoordinates) {
+      //Multisubject annotation
+      const setNX = noteData.nx || screenCoordinates[0][0] + noteData.dx;
+      const setNY = noteData.ny || screenCoordinates[0][1] + noteData.dy;
+      const notes = screenCoordinates.map((d, i) => {
+        const subjectNote = Object.assign({}, noteData, {
+          note: i === 0 ? noteData.note : { label: "" },
+          x: d[0],
+          y: d[1],
+          nx: setNX,
+          ny: setNY
+        });
+
+        return <noteData.type key={`multi-annotation-${i}`} {...subjectNote} />;
+      });
+
+      return <g events={eventListeners}>{notes}</g>;
+    }
+
     return <noteData.type events={eventListeners} {...noteData} />;
   }
 }
 
-Annotation.propTypes = {
+SemioticAnnotation.propTypes = {
   noteData: PropTypes.object
 };
 
-export default Annotation;
+export default SemioticAnnotation;
