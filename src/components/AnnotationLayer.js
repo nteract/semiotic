6f8b6da86b94a30781b64c1b66585d689bd7ca86
Elijah Meeks <elijahmeeks@gmail.com>
2018-01-19 21:50:13 -0800

    Working simple Labella example

--- src/components/AnnotationLayer.js
+++ src/components/AnnotationLayer.js
@@ -8,6 +8,7 @@ import {
 import PropTypes from "prop-types"
 import Legend from "./Legend"
 import Annotation from "./Annotation"
+import labella from "labella"
 
 function adjustedAnnotationKeyMapper(d) {
   return d.props.noteData.id || `${d.props.noteData.x}-${d.props.noteData.y}`
@@ -91,6 +92,93 @@ class AnnotationLayer extends React.Component {
     if (annotationProcessor.type === "bump") {
       const adjustedAnnotations = bumpAnnotations(adjustableAnnotations, props)
       return adjustedAnnotations
+    } else if (annotationProcessor.type === "labella") {
+      const labellaNodes = adjustableAnnotations.map((d, i) => {
+        return new labella.Node(d.props.noteData.y, 20)
+      })
+
+      const leftNodes = []
+      const rightNodes = []
+      const topNodes = []
+      const bottomNodes = []
+
+      adjustableAnnotations.forEach(aNote => {
+        const noteData = aNote.props.noteData
+        const leftDist = noteData.x
+        const rightDist = 700 - noteData.x
+        const topDist = noteData.y
+        const bottomDist = 700 - noteData.y
+
+        const minDist = Math.min(leftDist, rightDist, topDist, bottomDist)
+
+        if (leftDist === minDist) {
+          leftNodes.push(aNote)
+        } else if (rightDist === minDist) {
+          rightNodes.push(aNote)
+        } else if (topDist === minDist) {
+          topNodes.push(aNote)
+        } else {
+          bottomNodes.push(aNote)
+        }
+      })
+
+      const leftForce = new labella.Force({
+        minPos: 0,
+        maxPos: 700
+      })
+        .nodes(leftNodes.map(d => new labella.Node(d.props.noteData.y, 20)))
+        .compute()
+
+      const rightForce = new labella.Force({
+        minPos: 0,
+        maxPos: 700
+      })
+        .nodes(rightNodes.map(d => new labella.Node(d.props.noteData.y, 20)))
+        .compute()
+
+      const topForce = new labella.Force({
+        minPos: 0,
+        maxPos: 700
+      })
+        .nodes(topNodes.map(d => new labella.Node(d.props.noteData.x, 60)))
+        .compute()
+
+      const bottomForce = new labella.Force({
+        minPos: 0,
+        maxPos: 700
+      })
+        .nodes(bottomNodes.map(d => new labella.Node(d.props.noteData.x, 60)))
+        .compute()
+
+      const leftSortedNodes = leftForce.nodes()
+      const rightSortedNodes = rightForce.nodes()
+      const topSortedNodes = topForce.nodes()
+      const bottomSortedNodes = bottomForce.nodes()
+
+      leftNodes.forEach((note, i) => {
+        note.props.noteData.ny = leftSortedNodes[i].currentPos
+        note.props.noteData.nx = 20 + leftSortedNodes[i].layerIndex * 20
+      })
+
+      rightNodes.forEach((note, i) => {
+        note.props.noteData.ny = rightSortedNodes[i].currentPos
+        note.props.noteData.nx = 620 + rightSortedNodes[i].layerIndex * 20
+      })
+
+      topNodes.forEach((note, i) => {
+        note.props.noteData.nx = topSortedNodes[i].currentPos
+        note.props.noteData.ny = 20 + topSortedNodes[i].layerIndex * 20
+      })
+
+      bottomNodes.forEach((note, i) => {
+        note.props.noteData.nx = bottomSortedNodes[i].currentPos
+        note.props.noteData.ny = 620 + bottomSortedNodes[i].layerIndex * 20
+      })
+
+      console.log("bottomNodes", bottomNodes)
+      console.log("topNodes", topNodes)
+
+      return adjustableAnnotations
     } else if (annotationProcessor.type === "marginalia") {
       adjustableAnnotations.sort(
         (a, b) => a.props.noteData.y - b.props.noteData.y
