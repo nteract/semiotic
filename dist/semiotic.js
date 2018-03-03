49b1452b04ec0ab3b84dd90fb21aa495d754c3ca
Elijah Meeks <elijahmeeks@gmail.com>
2018-03-02 21:11:36 -0800

    Oh yeah

--- dist/semiotic.js
+++ dist/semiotic.js
@@ -45437,45 +45437,46 @@ MinimapNetworkFrame.propTypes = {
   renderBefore: propTypes.oneOfType([propTypes.array, propTypes.object])
 };
 
-var requestFrame = function () {
-  var window = this;
-  var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function fallbackRAF(func) {
-    return window.setTimeout(func, 20);
-  };
-  return function requestFrameFunction(func) {
-    return raf(func);
-  };
-}();
-
-var cancelFrame = function () {
-  var window = this;
-  var cancel = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.clearTimeout;
-  return function cancelFrameFunction(id) {
-    return cancel(id);
-  };
-}();
-
-function resizeListener(e) {
-  var win = e.target || e.srcElement;
-  if (win.__resizeRAF__) {
-    cancelFrame(win.__resizeRAF__);
-  }
-  win.__resizeRAF__ = requestFrame(function () {
-    var trigger = win.__resizeTrigger__;
-    trigger.__resizeListeners__.forEach(function (fn) {
-      fn.call(trigger, e);
-    });
-  });
-}
-
 var exports$1 = function exports(element, fn) {
   var window = this;
   var document = window.document;
   var isIE;
+  var requestFrame;
 
   var attachEvent = document.attachEvent;
   if (typeof navigator !== 'undefined') {
     isIE = navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/Edge/);
+  }
+
+  requestFrame = function () {
+    var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function fallbackRAF(func) {
+      return window.setTimeout(func, 20);
+    };
+    return function requestFrameFunction(func) {
+      return raf(func);
+    };
+  }();
+
+  var cancelFrame = function () {
+    var cancel = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.clearTimeout;
+    return function cancelFrameFunction(id) {
+      return cancel(id);
+    };
+  }();
+
+  function resizeListener(e) {
+    var win = e.target || e.srcElement;
+    if (win.__resizeRAF__) {
+      cancelFrame(win.__resizeRAF__);
+    }
+    win.__resizeRAF__ = requestFrame(function () {
+      var trigger = win.__resizeTrigger__;
+      if (trigger !== undefined) {
+        trigger.__resizeListeners__.forEach(function (fn) {
+          fn.call(trigger, e);
+        });
+      }
+    });
   }
 
   function objectLoad() {
@@ -45495,7 +45496,6 @@ var exports$1 = function exports(element, fn) {
       var obj = element.__resizeTrigger__ = document.createElement('object');
       obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1; opacity: 0;');
       obj.setAttribute('class', 'resize-sensor');
-      obj.setAttribute('tabIndex', '-1');
       obj.__resizeElement__ = element;
       obj.onload = objectLoad;
       obj.type = 'text/html';
@@ -45511,26 +45511,20 @@ var exports$1 = function exports(element, fn) {
   element.__resizeListeners__.push(fn);
 };
 
-var elementResizeEvent = typeof window === 'undefined' ? exports$1 : exports$1.bind(window);
-
 var unbind = function unbind(element, fn) {
   var attachEvent = document.attachEvent;
-  if (fn) {
-    element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(fn), 1);
-  } else {
-    element.__resizeListeners__ = [];
-  }
+  element.__resizeListeners__.splice(element.__resizeListeners__.indexOf(fn), 1);
   if (!element.__resizeListeners__.length) {
     if (attachEvent) {
       element.detachEvent('onresize', resizeListener);
     } else {
       element.__resizeTrigger__.contentDocument.defaultView.removeEventListener('resize', resizeListener);
-      delete element.__resizeTrigger__.contentDocument.defaultView.__resizeTrigger__;
       element.__resizeTrigger__ = !element.removeChild(element.__resizeTrigger__);
     }
-    delete element.__resizeListeners__;
   }
 };
+
+var elementResizeEvent = typeof window === 'undefined' ? exports$1 : exports$1.bind(window);
 
 elementResizeEvent.unbind = unbind;
 
