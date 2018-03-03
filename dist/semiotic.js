6f66460d06f48a6723329d0b220c2e76d9de1185
Elijah Meeks <elijahmeeks@gmail.com>
2018-03-02 21:22:23 -0800

    Tab Index on the right version?

--- dist/semiotic.js
+++ dist/semiotic.js
@@ -45496,6 +45496,7 @@ var exports$1 = function exports(element, fn) {
       var obj = element.__resizeTrigger__ = document.createElement('object');
       obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; pointer-events: none; z-index: -1; opacity: 0;');
       obj.setAttribute('class', 'resize-sensor');
+      obj.setAttribute('tabIndex', '-1');
       obj.__resizeElement__ = element;
       obj.onload = objectLoad;
       obj.type = 'text/html';
