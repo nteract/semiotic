93f08eb4130e2e5465db75957d624caa5061d580
Elijah Meeks <elijahmeeks@gmail.com>
2018-03-30 17:30:29 -0700

    Oh prettier

--- src/docs/components/DendrogramRaw.js
+++ src/docs/components/DendrogramRaw.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import { NetworkFrame } from '../../components'
-import { data } from '../sampledata/d3_api'
-import ProcessViz from './ProcessViz'
+import React from "react"
+import { NetworkFrame } from "../../components"
+import { data } from "../sampledata/d3_api"
+import ProcessViz from "./ProcessViz"
 
-const colors = ['#00a2ce', '#b6a756', '#4d430c', '#b3331d']
+const colors = ["#00a2ce", "#b6a756", "#4d430c", "#b3331d"]
 
 export default ({
-  annotation = 'rectangle',
-  type = 'dendrogram',
+  annotation = "rectangle",
+  type = "dendrogram",
   projection
 }) => {
   const hierarchicalChart = {
-    title: 'D3v3 API',
+    title: "D3v3 API",
     size: [700, 700],
     edges: data,
     nodeStyle: (d, i) => ({
       fill: colors[d.depth],
-      stroke: 'black',
+      stroke: "black",
       strokeOpacity: 0.25,
       fillOpacity: 0.25
     }),
@@ -25,46 +25,46 @@ export default ({
       stroke: colors[d.source.depth],
       opacity: 0.5
     }),
-    nodeIDAccessor: 'name',
-    hoverAnnotation: 'edge',
+    nodeIDAccessor: "name",
+    hoverAnnotation: "edge",
     networkType: {
       type,
       projection: projection,
       nodePadding: 1,
       forceManyBody: -15,
       edgeStrength: 1.5,
-      padding: type === 'treemap' ? 3 : type === 'circlepack' ? 2 : 0,
+      padding: type === "treemap" ? 3 : type === "circlepack" ? 2 : 0,
       hierarchySum: d => d.blockCalls
     },
     tooltipContent: d => {
-      return d.edge ? 
+      return d.edge ? (
         <div className="tooltip-content">
           <p>{d.edge.source.name}</p>
           <p>{d.edge.target.name}</p>
         </div>
-        : 
+      ) : (
         <div className="tooltip-content">
           {d.parent ? <p>{d.parent.data.name}</p> : undefined}
           <p>{d.data.name}</p>
         </div>
-      
+      )
     },
     annotations: [
       {
-        type: annotation === 'rectangle' ? 'enclose-rect' : 'enclose',
+        type: annotation === "rectangle" ? "enclose-rect" : "enclose",
         ids: [
-          'identity',
-          'linear',
-          'pow',
-          'category20',
-          'category20',
-          'log',
-          'sqrt',
-          'ordinal',
-          'threshold',
-          'quantize'
+          "identity",
+          "linear",
+          "pow",
+          "category20",
+          "category20",
+          "log",
+          "sqrt",
+          "ordinal",
+          "threshold",
+          "quantize"
         ],
-        label: 'Scales',
+        label: "Scales",
         padding: 5,
         dy: -150,
         nx: 660
@@ -79,9 +79,9 @@ export default ({
         width="560"
         height="315"
         src="https://www.youtube.com/embed/diE5ywpQNjU"
-        frameborder="0"
+        frameBorder="0"
         allow="autoplay; encrypted-media"
-        allowfullscreen
+        allowFullScreen
       />
       <ProcessViz frameSettings={hierarchicalChart} frameType="NetworkFrame" />
       <NetworkFrame {...hierarchicalChart} />
