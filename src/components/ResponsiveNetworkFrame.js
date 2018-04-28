ee3d3798ea3a908d16fa0a1de0a3a8343c0e21c7
Elijah Meeks <elijahmeeks@gmail.com>
2018-04-27 23:09:54 -0700

    Export weird frames separately

---
+++ src/components/ResponsiveNetworkFrame.js
@@ -0,0 +1,6 @@
+import React from "react"
+import PropTypes from "prop-types"
+import NetworkFrame from "./NetworkFrame"
+import createResponsiveFrame from "./ResponsiveFrame"
+
+export default createResponsiveFrame(NetworkFrame)
