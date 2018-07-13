85e38776ffaa1417e7562343393c4a02d7712d57
Elijah Meeks <elijahmeeks@gmail.com>
2018-07-12 23:30:15 -0700

    Move around some logging

--- src/components/data/multiAccessorUtils.js
+++ src/components/data/multiAccessorUtils.js
@@ -1,13 +1,6 @@
 export const findFirstAccessorValue = (accessorArray, data) => {
   for (let i = 0; i < accessorArray.length; i++) {
-    console.log(
-      " accessorArray[i](data)",
-      accessorArray[i],
-      accessorArray[i](data)
-    )
-    console.log("data", data)
     const valueCheck = accessorArray[i](data)
-    console.log("valueCheck", valueCheck, !isNaN(valueCheck))
     if (
       valueCheck !== undefined &&
       !Number.isNaN(valueCheck) &&
