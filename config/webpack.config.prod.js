f37b5eb7e023926cc232af312eb862408bd0dcc1
Elijah Meeks <elijahmeeks@gmail.com>
2018-09-08 19:24:42 -0700

    Try to process d3-delauney

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -142,7 +142,7 @@ module.exports = {
           // Process JS with Babel.
           {
             test: /\.(js|jsx|mjs)$/,
-            include: paths.appSrc,
+            include: [paths.appSrc, paths.delauneyPath],
             loader: require.resolve("babel-loader"),
             options: {
               compact: true
