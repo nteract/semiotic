d44cbf44fbbd1e55ee53858655203fee45ab77dd
Elijah Meeks <elijahmeeks@gmail.com>
2018-09-10 10:59:38 -0700

    Remove regenerator ref

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import babel from "rollup-plugin-babel"
 import commonjs from "rollup-plugin-commonjs"
 import builtins from "rollup-plugin-node-builtins"
 import replace from "rollup-plugin-replace"
-import regenerator from "rollup-plugin-regenerator"
 import nodent from "rollup-plugin-nodent"
 
 import flow from "rollup-plugin-flow"
