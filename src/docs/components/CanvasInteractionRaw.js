38bd932ab74da3feeec71395b6a3f91b0d5bd1ce
Susie Lu <slu@netflix.com>
2018-10-16 20:52:04 -0700

    remove diamonds.js from bundle

--- src/docs/components/CanvasInteractionRaw.js
+++ src/docs/components/CanvasInteractionRaw.js
@@ -1,7 +1,7 @@
 import React from "react"
-import diamondData from "../sampledata/diamonds"
 import { csvParse } from "d3-dsv"
 import { XYFrame } from "../../components"
+const ROOT = process.env.PUBLIC_URL
 
 const colors = [
   "#007190",
@@ -12,8 +12,6 @@ const colors = [
   "rgb(182, 167, 86)"
 ]
 
-const parsedDiamonds = []
-
 const cutHash = {
   "Ideal": colors[0],
   "Premium": colors[1],
@@ -23,48 +21,66 @@ const cutHash = {
   "Premium": colors[5]
 }
 
-csvParse(diamondData).forEach(d => {
-  parsedDiamonds.push({
-    y: +d.price,
-    x: +d.carat,
-    size: +d.table,
-    color: cutHash[d.cut],
-    clarity: d.clarity
-  })
-})
+export default class DecisionMatrixExample extends React.Component {
+  state = { parsedDiamonds: [] }
+  constructor(props) {
+    super(props)
 
-export default (
-  <div>
-    <XYFrame
-      points={parsedDiamonds}
-      size={[700, 700]}
-      xAccessor="x"
-      yAccessor="y"
-      pointStyle={d => ({ fill: d.color, fillOpacity: 0.9 })}
-      canvasPoints={true}
-      axes={[
-        { orient: "bottom", label: "Carat" },
-        {
-          label: "Price",
-          orient: "left",
-          tickFormat: d => `$${d / 1000}k`
-        }
-      ]}
-      margin={{ left: 75, bottom: 100, top: 10, right: 10 }}
-      hoverAnnotation={true}
-      tooltipContent={d => (
-        <div className="tooltip-content">
-          <p>Price: ${d.y}</p>
-          <p>Caret: {d.x}</p>
-          <p>
-            {d.coincidentPoints.length > 1 &&
-              `+${d.coincidentPoints.length - 1} more diamond${(d
-                .coincidentPoints.length > 2 &&
-                "s") ||
-                ""} here`}
-          </p>
-        </div>
-      )}
-    />
-  </div>
-)
+    fetch(`${ROOT}/sampledata/diamonds.csv`)
+      .then(response => response.text())
+      .then(data => {
+        // console.log(data)
+        const parsedDiamonds = []
+        csvParse(data).forEach(d => {
+          parsedDiamonds.push({
+            y: +d.price,
+            x: +d.carat,
+            size: +d.table,
+            color: cutHash[d.cut],
+            clarity: d.clarity
+          })
+        })
+        this.setState({ parsedDiamonds })
+      })
+  }
+
+  render() {
+    const { parsedDiamonds } = this.state
+    if (parsedDiamonds.length === 0) return <div>Loading...</div>
+    return (
+      <div>
+        <XYFrame
+          points={parsedDiamonds}
+          size={[700, 700]}
+          xAccessor="x"
+          yAccessor="y"
+          pointStyle={d => ({ fill: d.color, fillOpacity: 0.9 })}
+          canvasPoints={true}
+          axes={[
+            { orient: "bottom", label: "Carat" },
+            {
+              label: "Price",
+              orient: "left",
+              tickFormat: d => `$${d / 1000}k`
+            }
+          ]}
+          margin={{ left: 75, bottom: 100, top: 10, right: 10 }}
+          hoverAnnotation={true}
+          tooltipContent={d => (
+            <div className="tooltip-content">
+              <p>Price: ${d.y}</p>
+              <p>Caret: {d.x}</p>
+              <p>
+                {d.coincidentPoints.length > 1 &&
+                  `+${d.coincidentPoints.length - 1} more diamond${(d
+                    .coincidentPoints.length > 2 &&
+                    "s") ||
+                    ""} here`}
+              </p>
+            </div>
+          )}
+        />
+      </div>
+    )
+  }
+}
