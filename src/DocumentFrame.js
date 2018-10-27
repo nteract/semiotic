1c8dc9395d250243f721eedddf04f166dd85e184
Susie Lu <slu@netflix.com>
2018-10-27 10:47:25 -0700

    trimmed data in code viewer

--- src/DocumentFrame.js
+++ src/DocumentFrame.js
@@ -2,40 +2,128 @@ import React from "react"
 import { OrdinalFrame } from "semiotic"
 import { processNodes } from "./process"
 
-const objectToString = obj => {
+const objectToString = (obj, indent, trimmed) => {
   let newObj = "{ "
-
   const keys = Object.keys(obj),
     len = keys.length - 1
 
+  let spaces = ""
+  let x = 0
+  for (x; x <= indent - 2; x++) {
+    spaces += "  "
+  }
   keys.forEach((k, i) => {
     newObj += "\n    "
-    newObj += k + ": " + propertyToString(obj[k])
+    newObj += spaces
+    newObj += k + ": " + propertyToString(obj[k], indent + 1, trimmed)
     if (i !== len) newObj += ", "
   })
 
-  newObj += "\n  }"
+  newObj += `\n ${spaces} }`
 
   return newObj
 }
 
-const propertyToString = value => {
+const propertyToString = (value, indent, trimmed) => {
   let string
   const type = typeof value
   const isArray = Array.isArray(value)
-
+  let spaces = ""
+  let x = 0
+  for (x; x <= indent - 1; x++) {
+    spaces += "  "
+  }
   if (type === "function") {
     string = value.toString()
   } else if (type === "object" && !isArray) {
-    string = objectToString(value)
+    string = objectToString(value, indent, trimmed)
+  } else if (isArray) {
+    const arr = trimmed ? value.slice(0, 2) : value
+    string =
+      "[" +
+      arr.map(d => propertyToString(d, indent + 1, trimmed)) +
+      `${value.length > 2 ? `,\n${spaces}  ...\n${spaces}` : ""}]`
   } else {
     string = JSON.stringify(value)
-      .replace(/,{/g, ",\n    {")
-      .replace(/:/g, ": ")
-      .replace(/"coordinates"/g, '\n   "coordinates"')
   }
 
   return string
+}
+
+const getFunctionString = functions => {
+  let functionsString = ""
+
+  Object.keys(functions).forEach(d => {
+    functionsString += functions[d]
+    functionsString += "\n"
+  })
+
+  if (functionsString) functionsString += "\n"
+
+  return functionsString
+}
+
+const getFramePropsString = (frameProps, functions, overrideProps, trimmed) => {
+  const frameString = Object.keys(frameProps)
+    .map(d => {
+      const order = processNodes.findIndex(p => p.keys.indexOf(d) !== -1)
+
+      const { label } = processNodes[order]
+
+      return {
+        key: d,
+        value: frameProps[d],
+        label,
+        order:
+          order +
+          processNodes[order].keys.indexOf(d) / processNodes[order].keys.length
+      }
+    })
+    .sort((a, b) => {
+      return a.order - b.order
+    })
+
+  let framePropsString = "const frameProps = { ",
+    category
+
+  frameString.forEach((d, i) => {
+    if (i !== 0) framePropsString += "\n"
+
+    if (category !== d.label) {
+      framePropsString += "\n/* --- " + d.label + " --- */\n"
+      category = d.label
+    }
+
+    let string =
+      (functions[d.key] && (functions[d.key].name || d.key)) ||
+      overrideProps[d.key] ||
+      propertyToString(d.value, 0, trimmed)
+
+    framePropsString += `  ${d.key}: ${string}${(i !== frameString.length - 1 &&
+      ",") ||
+      ""}`
+  })
+
+  framePropsString += "\n}"
+  return framePropsString
+}
+
+const getCodeBlock = (frameName, functionsString, framePropsString) => {
+  return (
+    <code className="language-jsx">
+      {`import { ${frameName} } from "semiotic/lib/${frameName}"\n\n`}
+
+      {functionsString}
+
+      {framePropsString}
+      {"\n\n"}
+      {`export default () => {
+
+return <${frameName} {...frameProps} />
+
+}`}
+    </code>
+  )
 }
 
 const styles = {
@@ -70,7 +158,6 @@ class DocumentFrame extends React.Component {
   }
 
   onCopy() {
-    // this.copy.select()
     const text = this.copy.textContent
     const callback = e => {
       e.clipboardData.clearData()
@@ -79,16 +166,11 @@ class DocumentFrame extends React.Component {
       document.removeEventListener("copy", callback)
     }
     document.addEventListener("copy", callback)
-
     document.execCommand("copy")
-
-    // window.copy(this.copy.textContent)
-    // this.setState({ codeBlock: e.target.value })
   }
 
   render() {
     const {
-      // props,
       frameProps,
       type = OrdinalFrame,
       overrideProps = {},
@@ -96,57 +178,18 @@ class DocumentFrame extends React.Component {
     } = this.props
     const Frame = type
 
-    // let category =
-    const frameString = Object.keys(frameProps)
-      .map(d => {
-        const order = processNodes.findIndex(p => p.keys.indexOf(d) !== -1)
-
-        const { label } = processNodes[order]
-
-        return {
-          key: d,
-          value: frameProps[d],
-          label,
-          order:
-            order +
-            processNodes[order].keys.indexOf(d) /
-              processNodes[order].keys.length
-        }
-      })
-      .sort((a, b) => {
-        return a.order - b.order
-      })
-
-    let framePropsString = "const frameProps = { ",
-      category
-
-    frameString.forEach((d, i) => {
-      if (i !== 0) framePropsString += "\n"
-
-      if (category !== d.label) {
-        framePropsString += "\n/* --- " + d.label + " --- */\n"
-        category = d.label
-      }
-
-      let string =
-        (functions[d.key] && (functions[d.key].name || d.key)) ||
-        overrideProps[d.key] ||
-        propertyToString(d.value)
-
-      framePropsString += `  ${d.key}: ${string}${(i !==
-        frameString.length - 1 &&
-        ",") ||
-        ""}`
-    })
-
-    framePropsString += "\n}"
-
-    let functionsString = ""
-
-    Object.keys(functions).forEach(d => {
-      functionsString += functions[d]
-      functionsString += "\n"
-    })
+    const framePropsString = getFramePropsString(
+      frameProps,
+      functions,
+      overrideProps
+    )
+    const trimmedFramePropsString = getFramePropsString(
+      frameProps,
+      functions,
+      overrideProps,
+      true
+    )
+    const functionsString = getFunctionString(functions)
 
     const frameName = Frame.name
 
@@ -156,37 +199,13 @@ class DocumentFrame extends React.Component {
         ref={el => (this.copy = el)}
         style={hiddenStyle}
       >
-        <code className="language-jsx">
-          {`import { ${frameName} } from "semiotic/lib/${frameName}"\n\n`}
-
-          {functionsString && functionsString + "\n"}
-
-          {framePropsString}
-          {"\n\n"}
-          {`export default () => {
-
-  return <${frameName} {...frameProps} />
-
-}`}
-        </code>
+        {getCodeBlock(frameName, functionsString, framePropsString)}
       </pre>
     )
 
     const trimmedMarkdown = (
       <pre className="language-jxs" style={styles[this.state.codeBlock]}>
-        <code className="language-jsx">
-          {`import { ${frameName} } from "semiotic/lib/${frameName}"\n\n`}
-
-          {functionsString && functionsString + "\n"}
-
-          {framePropsString}
-          {"\n\n"}
-          {`export default () => {
-
-  return <${frameName} {...frameProps} />
-
-}`}
-        </code>
+        {getCodeBlock(frameName, functionsString, trimmedFramePropsString)}
       </pre>
     )
 
