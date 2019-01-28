a8bac82a651eebdc6626c1b86972b9ad8e339ffc
Elijah Meeks <elijahmeeks@gmail.com>
2019-01-28 14:57:16 -0800

    Support hierarchical data passed as an edge list

--- src/components/svg/networkDrawing.js
+++ src/components/svg/networkDrawing.js
@@ -247,9 +247,7 @@ export const dagreEdgeGenerator = direction => {
           (a, b) => b.weight - a.weight
         )
         return (
-          <g
-          key={`${key}`}
-          >
+          <g key={`${key}`}>
             {ribbonGenerator({
               points: d.points,
               multiple: d.parallelEdges
@@ -473,36 +471,34 @@ export const matrixNodeGenerator = (size, nodes) => {
             baseMarkProps={baseMarkProps}
           />
         )}
-        {showLine &&
-          i === nodes.length - 1 && (
-            <Mark
-              markType="line"
-              stroke="black"
-              x1={0}
-              x2={gridSize - stepSize / 2}
-              y1={d.y + stepSize / 2}
-              y2={d.y + stepSize / 2}
-              style={style}
-              renderMode={renderModeValue}
-              forceUpdate={true}
-              baseMarkProps={baseMarkProps}
-            />
-          )}
-        {showLine &&
-          i === nodes.length - 1 && (
-            <Mark
-              markType="line"
-              stroke="black"
-              y1={0}
-              y2={gridSize - stepSize / 2}
-              x1={d.y + stepSize / 2}
-              x2={d.y + stepSize / 2}
-              style={style}
-              renderMode={renderModeValue}
-              forceUpdate={true}
-              baseMarkProps={baseMarkProps}
-            />
-          )}
+        {showLine && i === nodes.length - 1 && (
+          <Mark
+            markType="line"
+            stroke="black"
+            x1={0}
+            x2={gridSize - stepSize / 2}
+            y1={d.y + stepSize / 2}
+            y2={d.y + stepSize / 2}
+            style={style}
+            renderMode={renderModeValue}
+            forceUpdate={true}
+            baseMarkProps={baseMarkProps}
+          />
+        )}
+        {showLine && i === nodes.length - 1 && (
+          <Mark
+            markType="line"
+            stroke="black"
+            y1={0}
+            y2={gridSize - stepSize / 2}
+            x1={d.y + stepSize / 2}
+            x2={d.y + stepSize / 2}
+            style={style}
+            renderMode={renderModeValue}
+            forceUpdate={true}
+            baseMarkProps={baseMarkProps}
+          />
+        )}
         {showText && (
           <text x={0} y={d.y + stepSize / 5} {...textProps}>
             {d.id}
@@ -971,4 +967,101 @@ export function circularAreaLink(link) {
         ]
 
   return linkGenerator(xyForLink)
+}
+
+const hierarchyDecorator = (hierarchy, hashEntries, nodeIDAccessor, nodes) => {
+  if (hierarchy.children) {
+    hierarchy.children.forEach(child => {
+      const theseEntries = hashEntries.filter(entry => entry[1] === child.id)
+      theseEntries.forEach(entry => {
+        const idNode =
+          nodes.find(node => nodeIDAccessor(node) === entry[0]) || {}
+
+        child.childHash[entry[0]] = {
+          id: entry[0],
+          children: [],
+          childHash: {},
+          ...idNode
+        }
+        child.children.push(child.childHash[entry[0]])
+      })
+      if (child.children.length > 0) {
+        hierarchyDecorator(child, hashEntries, nodeIDAccessor, nodes)
+      }
+    })
+  }
+}
+
+export const softStack = (
+  edges,
+  nodes,
+  sourceAccessor,
+  targetAccessor,
+  nodeIDAccessor
+) => {
+  let hierarchy = { id: "root-generated", children: [], childHash: {} }
+  const discoveredHierarchyHash = {}
+  const targetToSourceHash = {}
+  let hasLogicalRoot = true
+  let isHierarchical = true
+
+  for (let i = 0; i < edges.length; i++) {
+    const edge = edges[i]
+
+    const source = sourceAccessor(edge)
+    const target = targetAccessor(edge)
+    const sourceID =
+      typeof source === "object" ? nodeIDAccessor(source) : source
+    const targetID =
+      typeof target === "object" ? nodeIDAccessor(target) : target
+
+    targetToSourceHash[targetID] = sourceID
+
+    if (!discoveredHierarchyHash[sourceID]) {
+      discoveredHierarchyHash[sourceID] = targetID
+    } else {
+      isHierarchical = false
+      break
+    }
+  }
+  if (isHierarchical) {
+    const hashEntries = Object.entries(discoveredHierarchyHash)
+    hashEntries.forEach(entry => {
+      const target = entry[1]
+      if (!discoveredHierarchyHash[target]) {
+        discoveredHierarchyHash[target] = "root-generated"
+        const idNode = nodes.find(node => nodeIDAccessor(node) === target) || {}
+        hierarchy.childHash[target] = {
+          id: target,
+          children: [],
+          childHash: {},
+          ...idNode
+        }
+        hierarchy.children.push(hierarchy.childHash[target])
+      }
+    })
+
+    hierarchyDecorator(hierarchy, hashEntries, nodeIDAccessor, nodes)
+
+    nodes.forEach(node => {
+      const nodeID = nodeIDAccessor(node)
+      if (!discoveredHierarchyHash[nodeID] && !targetToSourceHash[nodeID]) {
+        hierarchy.children.push({
+          id: nodeID,
+          children: [],
+          childHash: {},
+          ...node
+        })
+      }
+    })
+
+    if (hierarchy.children.length === 1) {
+      hierarchy = hierarchy.children[0]
+      hasLogicalRoot = false
+    }
+
+    return { hierarchy, isHierarchical: true, hasLogicalRoot }
+  }
+
+  return { hierarchy: {}, isHierarchical: false, hasLogicalRoot: false }
 }
