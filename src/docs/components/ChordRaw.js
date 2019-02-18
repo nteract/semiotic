ba206c8d67d2a4e76929e03143f0c370690e464e
Elijah Meeks <elijahmeeks@gmail.com>
2019-02-18 11:33:03 -0800

    Fix chord labels
    chord wasn't using adjustedSize so if you passed a margin it didn't change the chord and would offset the labels because the labels were being placed in a margin-adjusted layer

--- src/docs/components/ChordRaw.js
+++ src/docs/components/ChordRaw.js
@@ -3,44 +3,104 @@ import { NetworkFrame } from "../../components"
 import ProcessViz from "./ProcessViz"
 
 const dematrixifiedEdges = [
-  { source: "a", target: "a", value: 11975 },
-  { source: "a", target: "b", value: 5871 },
-  { source: "a", target: "c", value: 8916 },
-  { source: "a", target: "d", value: 2868 },
-  { source: "b", target: "a", value: 1951 },
-  { source: "b", target: "b", value: 10048 },
-  { source: "b", target: "c", value: 2060 },
-  { source: "b", target: "d", value: 6171 },
-  { source: "c", target: "a", value: 8010 },
-  { source: "c", target: "b", value: 16145 },
-  { source: "c", target: "c", value: 8090 },
-  { source: "c", target: "d", value: 8045 },
-  { source: "d", target: "a", value: 1013 },
-  { source: "d", target: "b", value: 990 },
-  { source: "d", target: "c", value: 940 },
-  { source: "d", target: "d", value: 6907 }
+  { source: "apples are amazing", target: "apples are amazing", value: 11975 },
+  {
+    source: "apples are amazing",
+    target: "bananas become boring",
+    value: 5871
+  },
+  {
+    source: "apples are amazing",
+    target: "cantaloupes can crush cars",
+    value: 8916
+  },
+  {
+    source: "apples are amazing",
+    target: "dragonfruit don't dry darkly",
+    value: 2868
+  },
+  {
+    source: "bananas become boring",
+    target: "apples are amazing",
+    value: 1951
+  },
+  {
+    source: "bananas become boring",
+    target: "bananas become boring",
+    value: 10048
+  },
+  {
+    source: "bananas become boring",
+    target: "cantaloupes can crush cars",
+    value: 2060
+  },
+  {
+    source: "bananas become boring",
+    target: "dragonfruit don't dry darkly",
+    value: 6171
+  },
+  {
+    source: "cantaloupes can crush cars",
+    target: "apples are amazing",
+    value: 8010
+  },
+  {
+    source: "cantaloupes can crush cars",
+    target: "bananas become boring",
+    value: 16145
+  },
+  {
+    source: "cantaloupes can crush cars",
+    target: "cantaloupes can crush cars",
+    value: 8090
+  },
+  {
+    source: "cantaloupes can crush cars",
+    target: "dragonfruit don't dry darkly",
+    value: 8045
+  },
+  {
+    source: "dragonfruit don't dry darkly",
+    target: "apples are amazing",
+    value: 1013
+  },
+  {
+    source: "dragonfruit don't dry darkly",
+    target: "bananas become boring",
+    value: 990
+  },
+  {
+    source: "dragonfruit don't dry darkly",
+    target: "cantaloupes can crush cars",
+    value: 940
+  },
+  {
+    source: "dragonfruit don't dry darkly",
+    target: "dragonfruit don't dry darkly",
+    value: 6907
+  }
 ]
 
 const colors = ["#00a2ce", "#4d430c", "#b3331d", "#b6a756"]
 
 const nodes = [
   {
-    id: "a",
+    id: "apples are amazing",
     color: "orange"
   },
   {
-    id: "b",
+    id: "bananas become boring",
     color: "purple"
   },
   {
-    id: "c",
+    id: "cantaloupes can crush cars",
     color: "red"
   }
 ]
 
 export default ({ padAngle = 0.01, annotations }) => {
   const chordChart = {
-    size: [700, 400],
+    size: [700, 700],
     edges: dematrixifiedEdges,
     nodes: nodes,
     nodeStyle: d => ({ fill: d.color || colors[d.index], stroke: "black" }),
@@ -58,7 +118,9 @@ export default ({ padAngle = 0.01, annotations }) => {
     ],
     annotations: annotations,
     edgeWidthAccessor: "value",
-    networkType: { type: "chord", padAngle }
+    networkType: { type: "chord", padAngle },
+    nodeLabels: true,
+    margin: 50
   }
   return (
     <div>
