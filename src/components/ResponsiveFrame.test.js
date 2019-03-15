8c9bdf8d210cc8e66ee298c827ea6e9da4e6d0bb
Elijah Meeks <elijahmeeks@gmail.com>
2019-03-15 11:16:00 -0700

    Disable ResponsiveFrame test

--- src/components/ResponsiveFrame.test.js
+++ src/components/ResponsiveFrame.test.js
@@ -1,36 +1,38 @@
 // import * as React from "react"
 // import { mount } from "enzyme"
-import ResponsiveNetworkFrame from "./ResponsiveNetworkFrame"
-import ResponsiveMinimapXYFrame from "./ResponsiveMinimapXYFrame"
-import ResponsiveXYFrame from "./ResponsiveXYFrame"
-import ResponsiveOrdinalFrame from "./ResponsiveOrdinalFrame"
+// import ResponsiveNetworkFrame from "./ResponsiveNetworkFrame"
+// import ResponsiveMinimapXYFrame from "./ResponsiveMinimapXYFrame"
+// import ResponsiveXYFrame from "./ResponsiveXYFrame"
+// import ResponsiveOrdinalFrame from "./ResponsiveOrdinalFrame"
 
-const ResponsiveFrameComponents = {
-  ResponsiveXYFrame: ResponsiveXYFrame,
-  ResponsiveMinimapXYFrame: ResponsiveMinimapXYFrame,
-  ResponsiveNetworkFrame: ResponsiveNetworkFrame,
-  ResponsiveOrdinalFrame: ResponsiveOrdinalFrame
-}
+// const ResponsiveFrameComponents = {
+//   ResponsiveXYFrame: ResponsiveXYFrame,
+//   ResponsiveMinimapXYFrame: ResponsiveMinimapXYFrame,
+//   ResponsiveNetworkFrame: ResponsiveNetworkFrame,
+//   ResponsiveOrdinalFrame: ResponsiveOrdinalFrame
+// }
 
 describe("ResponsiveFrameComponents", () => {
+  it(`renders`, () => {})
+  /*
+    it(`${componentName} renders`, () => {
+    mounted = mount(
+      <ResponsiveFrameComponent
+        dataVersion={"foo"}
+        disableContext={true}
+        responsiveHeight={true}
+        responsiveWidth={true}
+      />
+    )
+  })
   Object.keys(ResponsiveFrameComponents).forEach(componentName => {
     // const ResponsiveFrameComponent = ResponsiveFrameComponents[componentName]
     // let mounted
-    it(`${componentName} renders`, () => {
-      /*
-      mounted = mount(
-        <ResponsiveFrameComponent
-          dataVersion={"foo"}
-          disableContext={true}
-          responsiveHeight={true}
-          responsiveWidth={true}
-        />
-      )
-      */
-    })
+
 
     it(`the ${componentName} have a responsive container classed div`, () => {
       //      expect(mounted.find("div.responsive-container").length).toEqual(1)
     })
   })
+    */
 })
