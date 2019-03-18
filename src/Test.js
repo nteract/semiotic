f840697b809cef1d31d5485e8903ac0229ed7980
slu <slu@netflix.com>
2019-03-18 13:54:55 -0700

    update example so it can be copy and pasted

--- src/Test.js
+++ src/Test.js
@@ -1,62 +1,194 @@
 import React from "react"
-import OrdinalFrame from "semiotic/lib/OrdinalFrame"
-import { scaleSqrt } from "d3-scale"
-const theme = [
-  "#ac58e5",
-  "#E0488B",
-  "#9fd0cb",
-  "#e0d33a",
-  "#7566ff",
-  "#533f82",
-  "#7a255d",
-  "#365350",
-  "#a19a11",
-  "#3f4482"
-]
+import XYFrame from "semiotic/lib/XYFrame"
 const frameProps = {
-  data: [
-    { user: "Jason", tweets: 40, retweets: 5, favorites: 15 },
-    { user: "Susie", tweets: 5, retweets: 25, favorites: 100 },
-    { user: "Matt", tweets: 20, retweets: 25, favorites: 50 },
-    { user: "Betty", tweets: 30, retweets: 20, favorites: 10 },
-    { user: "Ian", tweets: 5, retweets: 45, favorites: 100 },
-    { user: "Noah", tweets: 10, retweets: 5, favorites: 15 },
-    { user: "Shirley", tweets: 20, retweets: 25, favorites: 50 },
-    { user: "Rachel", tweets: 30, retweets: 20, favorites: 10 },
-    { user: "Nadieh", tweets: 30, retweets: 20, favorites: 15 },
-    { user: "Jim", tweets: 20, retweets: 25, favorites: 50 },
-    { user: "Zan", tweets: 5, retweets: 32, favorites: 100 },
-    { user: "Shelby", tweets: 30, retweets: 20, favorites: 10 }
+  lines: [
+    {
+      title: "Curve A",
+      forecast: undefined,
+      data: [
+        { month: 1, pct: 100 },
+        { month: 2, pct: 30 },
+        { month: 3, pct: 25 },
+        { month: 4, pct: 23 },
+        { month: 5, pct: 22 },
+        { month: 6, pct: 20 },
+        { month: 7, pct: 24 },
+        { month: 8, pct: 23 },
+        { month: 9, pct: 24 },
+        { month: 10, pct: 25 },
+        { month: 11, pct: 20 },
+        { month: 12, pct: 22 },
+        { month: 13, pct: 21 },
+        { month: 14, pct: 23 },
+        { month: 15, pct: 21 },
+        { month: 16 },
+        { month: 17 },
+        { month: 18 }
+      ]
+    },
+    {
+      title: "Curve A",
+      forecast: "lower",
+      data: [
+        { month: 1 },
+        { month: 2 },
+        { month: 3 },
+        { month: 4 },
+        { month: 5 },
+        { month: 6 },
+        { month: 7 },
+        { month: 8 },
+        { month: 9 },
+        { month: 10 },
+        { month: 11 },
+        { month: 12 },
+        { month: 13 },
+        { month: 14 },
+        { month: 15, pct: 21 },
+        { month: 16, pct: 18 },
+        { month: 17, pct: 15 },
+        { month: 18, pct: 10 }
+      ]
+    },
+    {
+      title: "Curve A",
+      forecast: "mean",
+      data: [
+        { month: 1 },
+        { month: 2 },
+        { month: 3 },
+        { month: 4 },
+        { month: 5 },
+        { month: 6 },
+        { month: 7 },
+        { month: 8 },
+        { month: 9 },
+        { month: 10 },
+        { month: 11 },
+        { month: 12 },
+        { month: 13 },
+        { month: 14 },
+        { month: 15, pct: 21 },
+        { month: 16, pct: 22 },
+        { month: 17, pct: 23 },
+        { month: 18, pct: 25 }
+      ]
+    },
+    {
+      title: "Curve A",
+      forecast: "upper",
+      data: [
+        { month: 1 },
+        { month: 2 },
+        { month: 3 },
+        { month: 4 },
+        { month: 5 },
+        { month: 6 },
+        { month: 7 },
+        { month: 8 },
+        { month: 9 },
+        { month: 10 },
+        { month: 11 },
+        { month: 12 },
+        { month: 13 },
+        { month: 14 },
+        { month: 15, pct: 21 },
+        { month: 16, pct: 25 },
+        { month: 17, pct: 30 },
+        { month: 18, pct: 38 }
+      ]
+    }
   ],
-  size: [400, 400],
-  margin: 70,
-  type: "bar",
-  projection: "radial",
-
-  rScaleType: scaleSqrt(),
-  oAccessor: "user",
-  rAccessor: ["tweets", "retweets", "favorites"],
-  style: d => {
-    return {
-      fill: theme[d.rIndex],
-      stroke: "white"
+  size: [700, 400],
+  margin: { left: 80, bottom: 50, right: 10, top: 40 },
+  defined: function defined(d) {
+    return d.pct !== undefined
+  },
+  xAccessor: "month",
+  yAccessor: "pct",
+  yExtent: [0, 100],
+  xExtent: [0, 20],
+  lineDataAccessor: "data",
+  pointStyle: { fill: "none", stroke: "none" },
+  lineStyle: d => {
+    let baseStyles = { stroke: "#E0488B", strokeWidth: "3px" }
+    if (!d.forecast) {
+      return baseStyles
+    } else if (d.forecast === "mean") {
+      return { ...baseStyles, strokeDasharray: "5px" }
+    } else {
+      return { strokeWidth: "0px" }
     }
   },
-  title: "Tweets",
-  axes: true,
-  oSort: function oSort(a, b, c, d) {
+  title: (
+    <text textAnchor="middle">Uncertainty Visualization (Time Series)</text>
+  ),
+  axes: [
+    {
+      orient: "left",
+      tickFormat: function tickFormat(d, i) {
+        return i === 0 ? null : d + "%"
+      },
+      label: {
+        name: "Decay (%)",
+        position: { anchor: "middle" },
+        locationDistance: 55
+      }
+    },
+    {
+      orient: "bottom",
+      ticks: 10,
+      tickFormat: function tickFormat(d, i) {
+        return i === 0 ? "Months" : d
+      }
+    }
+  ],
+  showLinePoints: true,
+  hoverAnnotation: true,
+  annotations: [
+    {
+      type: "area",
+      className: "uncertainty_cone",
+      coordinates: [
+        { month: 15, pct: 21 },
+        { month: 16, pct: 18 },
+        { month: 17, pct: 15 },
+        { month: 18, pct: 10 },
+        { month: 18, pct: 38 },
+        { month: 17, pct: 30 },
+        { month: 16, pct: 25 },
+        { month: 15, pct: 21 }
+      ]
+    }
+  ],
+  tooltipContent: d => {
+    let projectionString = null
+    if (d.parentLine.forecast) {
+      projectionString = (
+        <div
+          style={{ color: "#E0488B", fontWeight: "bold", marginBottom: "5px" }}
+        >{`* ${d.parentLine.forecast} forecast *`}</div>
+      )
+    }
     return (
-      c[0].tweets +
-      c[0].retweets +
-      c[0].favorites -
-      d[0].tweets -
-      d[0].retweets -
-      d[0].favorites
+      <div
+        style={{
+          border: `thin solid ${"#E0488B"}`,
+          backgroundColor: "white",
+          width: "100px",
+          textAlign: "center",
+          padding: "10px",
+          verticalAlign: "middle"
+        }}
+      >
+        {projectionString}
+        <div>{`Month: ${d.month}`}</div>
+        <div>{`Decay: ${d.pct}%`}</div>
+      </div>
     )
-  },
-  oLabel: true
+  }
 }
 
 export default () => {
-  return <OrdinalFrame {...frameProps} />
+  return <XYFrame {...frameProps} />
 }
--- src/examples/UncertaintyVisualization.js
+++ src/examples/UncertaintyVisualization.js
@@ -4,178 +4,249 @@ import { XYFrame } from "semiotic"
 import theme from "../theme"
 import MarkdownText from "../MarkdownText"
 
-const linesData = [{
-  title:'Curve A',
-  forecast:undefined,
-  data: [
-    {month:1,pct:100},
-    {month:2,pct:30},
-    {month:3,pct:25},
-    {month:4,pct:23},
-    {month:5,pct:22},
-    {month:6,pct:20},
-    {month:7,pct:24},
-    {month:8,pct:23},
-    {month:9,pct:24},
-    {month:10,pct:25},
-    {month:11,pct:20},
-    {month:12,pct:22},
-    {month:13,pct:21},
-    {month:14,pct:23},
-    {month:15,pct:21},
-    {month:16,pct:null},
-    {month:17,pct:null},
-    {month:18,pct:null},
-  ]
-},{
-  title:'Curve A',
-  forecast:'lower',
-  data: [
-    {month:1,pct:null},
-    {month:2,pct:null},
-    {month:3,pct:null},
-    {month:4,pct:null},
-    {month:5,pct:null},
-    {month:6,pct:null},
-    {month:7,pct:null},
-    {month:8,pct:null},
-    {month:9,pct:null},
-    {month:10,pct:null},
-    {month:11,pct:null},
-    {month:12,pct:null},
-    {month:13,pct:null},
-    {month:14,pct:null},
-    {month:15,pct:21},
-    {month:16,pct:18},
-    {month:17,pct:15},
-    {month:18,pct:10},
-  ]
-},{
-  title:'Curve A',
-  forecast:'mean',
-  data: [
-    {month:1,pct:null},
-    {month:2,pct:null},
-    {month:3,pct:null},
-    {month:4,pct:null},
-    {month:5,pct:null},
-    {month:6,pct:null},
-    {month:7,pct:null},
-    {month:8,pct:null},
-    {month:9,pct:null},
-    {month:10,pct:null},
-    {month:11,pct:null},
-    {month:12,pct:null},
-    {month:13,pct:null},
-    {month:14,pct:null},
-    {month:15,pct:21},
-    {month:16,pct:22},
-    {month:17,pct:23},
-    {month:18,pct:25},
-  ]
-},
-{
-  title:'Curve A',
-  forecast:'upper',
-  data: [
-    {month:1,pct:null},
-    {month:2,pct:null},
-    {month:3,pct:null},
-    {month:4,pct:null},
-    {month:5,pct:null},
-    {month:6,pct:null},
-    {month:7,pct:null},
-    {month:8,pct:null},
-    {month:9,pct:null},
-    {month:10,pct:null},
-    {month:11,pct:null},
-    {month:12,pct:null},
-    {month:13,pct:null},
-    {month:14,pct:null},
-    {month:15,pct:21},
-    {month:16,pct:25},
-    {month:17,pct:30},
-    {month:18,pct:38},
-  ]
-}]
+const linesData = [
+  {
+    title: "Curve A",
+    forecast: undefined,
+    data: [
+      { month: 1, pct: 100 },
+      { month: 2, pct: 30 },
+      { month: 3, pct: 25 },
+      { month: 4, pct: 23 },
+      { month: 5, pct: 22 },
+      { month: 6, pct: 20 },
+      { month: 7, pct: 24 },
+      { month: 8, pct: 23 },
+      { month: 9, pct: 24 },
+      { month: 10, pct: 25 },
+      { month: 11, pct: 20 },
+      { month: 12, pct: 22 },
+      { month: 13, pct: 21 },
+      { month: 14, pct: 23 },
+      { month: 15, pct: 21 },
+      { month: 16 },
+      { month: 17 },
+      { month: 18 }
+    ]
+  },
+  {
+    title: "Curve A",
+    forecast: "lower",
+    data: [
+      { month: 1 },
+      { month: 2 },
+      { month: 3 },
+      { month: 4 },
+      { month: 5 },
+      { month: 6 },
+      { month: 7 },
+      { month: 8 },
+      { month: 9 },
+      { month: 10 },
+      { month: 11 },
+      { month: 12 },
+      { month: 13 },
+      { month: 14 },
+      { month: 15, pct: 21 },
+      { month: 16, pct: 18 },
+      { month: 17, pct: 15 },
+      { month: 18, pct: 10 }
+    ]
+  },
+  {
+    title: "Curve A",
+    forecast: "mean",
+    data: [
+      { month: 1 },
+      { month: 2 },
+      { month: 3 },
+      { month: 4 },
+      { month: 5 },
+      { month: 6 },
+      { month: 7 },
+      { month: 8 },
+      { month: 9 },
+      { month: 10 },
+      { month: 11 },
+      { month: 12 },
+      { month: 13 },
+      { month: 14 },
+      { month: 15, pct: 21 },
+      { month: 16, pct: 22 },
+      { month: 17, pct: 23 },
+      { month: 18, pct: 25 }
+    ]
+  },
+  {
+    title: "Curve A",
+    forecast: "upper",
+    data: [
+      { month: 1 },
+      { month: 2 },
+      { month: 3 },
+      { month: 4 },
+      { month: 5 },
+      { month: 6 },
+      { month: 7 },
+      { month: 8 },
+      { month: 9 },
+      { month: 10 },
+      { month: 11 },
+      { month: 12 },
+      { month: 13 },
+      { month: 14 },
+      { month: 15, pct: 21 },
+      { month: 16, pct: 25 },
+      { month: 17, pct: 30 },
+      { month: 18, pct: 38 }
+    ]
+  }
+]
 
-let annotations = [{
-    type:"area",
-    className:'uncertainty_cone',
+let annotations = [
+  {
+    type: "area",
+    className: "uncertainty_cone",
     coordinates: linesData
       //Take only upper and lower datasets
-      .filter(d=>d.forecast && d.forecast !== 'mean')
+      .filter(d => d.forecast && d.forecast !== "mean")
       //ensure lower data set comes before upper data set
-      .sort((a,b)=>{ return ('' + a.forecast).localeCompare(b.forecast) })
-      .map((d,i)=>{
+      .sort((a, b) => {
+        return ("" + a.forecast).localeCompare(b.forecast)
+      })
+      .map((d, i) => {
         //Ensure sorted month order (desc) and drop all non-forecasted values
-        let tmpArr = d.data.sort((a,b)=>a.month-b.month).filter(x=>x.pct !== null)
+        let tmpArr = d.data
+          .sort((a, b) => a.month - b.month)
+          .filter(x => x.pct !== undefined)
         //If we're switching from lower to upper, reverse the coordinates orders to close the uncertainty polygon
-        if(i%2===1){
+        if (i % 2 === 1) {
           tmpArr = tmpArr.reverse()
         }
         return tmpArr
         //Merge the two coordinate arrays into one to create final polygon
-      }).flat()
-  }]
-
+      })
+      .flat()
+  }
+]
 
 const frameProps = {
-  title: (<text textAnchor="middle">Uncertainty Visualization (Time Series)</text>),
+  title: (
+    <text textAnchor="middle">Uncertainty Visualization (Time Series)</text>
+  ),
   size: [700, 400],
   lines: linesData,
-  lineDataAccessor: d => d.data,
-  lineStyle:(d)=>{
-    let baseStyles = {stroke:theme[1], strokeWidth:'3px'}
-    if(!d.forecast){ return baseStyles}
-    else if(d.forecast === 'mean'){
-      return { ...baseStyles, strokeDasharray: '5px'}
-    }
-    else{
-      return {strokeWidth:'0px'}
+  lineDataAccessor: "data",
+  lineStyle: d => {
+    let baseStyles = { stroke: theme[1], strokeWidth: "3px" }
+    if (!d.forecast) {
+      return baseStyles
+    } else if (d.forecast === "mean") {
+      return { ...baseStyles, strokeDasharray: "5px" }
+    } else {
+      return { strokeWidth: "0px" }
     }
   },
-  showLinePoints:true,
-  pointStyle: {fill: 'none', stroke:'none'},
+  showLinePoints: true,
+  pointStyle: { fill: "none", stroke: "none" },
   hoverAnnotation: true,
-  xAccessor: d => d.month,
-  yAccessor: d => d.pct,
-  yExtent:[0,100],
-  defined:d => d.pct !== null,
-  xExtent:[0,20],
+  xAccessor: "month",
+  yAccessor: "pct",
+  yExtent: [0, 100],
+  defined: d => d.pct !== undefined,
+  xExtent: [0, 20],
   margin: { left: 80, bottom: 50, right: 10, top: 40 },
-  annotations:annotations,
+  annotations: annotations,
   axes: [
     {
       orient: "left",
-      tickFormat: (d,i) => {return i === 0 ? null : `${d}%`},
+      tickFormat: (d, i) => {
+        return i === 0 ? null : `${d}%`
+      },
       label: {
-        name: 'Decay (%)',
+        name: "Decay (%)",
         position: {
-          anchor: 'middle',
+          anchor: "middle"
         },
-        locationDistance: 55,
+        locationDistance: 55
       }
     },
     {
       orient: "bottom",
-      ticks:10,
-      tickFormat: (d, i) => { return i === 0 ? 'Months' : d }
+      ticks: 10,
+      tickFormat: (d, i) => {
+        return i === 0 ? "Months" : d
+      }
     }
   ],
-  tooltipContent:(d)=>{
+  tooltipContent: d => {
     let projectionString = null
-    if(d.parentLine.forecast){
-      projectionString= <div style={{color: theme[1], fontWeight:'bold',marginBottom:'5px'}}>{`* ${d.parentLine.forecast} forecast *`}</div>
+    if (d.parentLine.forecast) {
+      projectionString = (
+        <div
+          style={{ color: theme[1], fontWeight: "bold", marginBottom: "5px" }}
+        >{`* ${d.parentLine.forecast} forecast *`}</div>
+      )
     }
-    return <div 
-      style={{border:`thin solid ${theme[1]}`,backgroundColor:'white', width:'100px', textAlign: 'center', padding: '10px', verticalAlign:'middle'}}>
+    return (
+      <div
+        style={{
+          border: `thin solid ${theme[1]}`,
+          backgroundColor: "white",
+          width: "100px",
+          textAlign: "center",
+          padding: "10px",
+          verticalAlign: "middle"
+        }}
+      >
         {projectionString}
         <div>{`Month: ${d.month}`}</div>
         <div>{`Decay: ${d.pct}%`}</div>
       </div>
-  },
- 
+    )
+  }
+}
+
+const overrideProps = {
+  lineStyle: `d => {
+    let baseStyles = { stroke: theme[1], strokeWidth: "3px" }
+    if (!d.forecast) {
+      return baseStyles
+    } else if (d.forecast === "mean") {
+      return { ...baseStyles, strokeDasharray: "5px" }
+    } else {
+      return { strokeWidth: "0px" }
+    }
+  }`,
+  title: `(
+    <text textAnchor="middle">Uncertainty Visualization (Time Series)</text>
+  )`,
+  tooltipContent: ` d => {
+    let projectionString = null
+    if (d.parentLine.forecast) {
+      projectionString = (
+        <div
+          style={{ color: theme[1], fontWeight: "bold", marginBottom: "5px" }}
+        >{\`* \${d.parentLine.forecast} forecast *\`}</div>
+      )
+    }
+    return (
+      <div
+        style={{
+          border: \`thin solid \${theme[1]}\`,
+          backgroundColor: "white",
+          width: "100px",
+          textAlign: "center",
+          padding: "10px",
+          verticalAlign: "middle"
+        }}
+      >
+        {projectionString}
+        <div>{\`Month: \${d.month}\`}</div>
+        <div>{\`Decay: \${d.pct}%\`}</div>
+      </div>
+    )
+  }`
 }
 
 const UncertaintyViz = () => {
@@ -196,10 +267,10 @@ This example stitches together four separate lines
       <DocumentFrame
         frameProps={frameProps}
         type={XYFrame}
+        overrideProps={overrideProps}
         useExpanded
         pre={``}
       />
-
     </div>
   )
 }
