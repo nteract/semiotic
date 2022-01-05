5669702c59e565cce7da92fbfb1430317d8bd0e2
Alexey Raspopov <oleksii.raspopov@noteable.io>
2022-01-05 11:43:29 -0500

    use effect is not applicable here

--- docs/src/useScrollRestoration.js
+++ docs/src/useScrollRestoration.js
@@ -1,11 +1,8 @@
-import { useLayoutEffect } from "react"
 import { useNavigationType } from "react-router-dom"
 
 export function useScrollRestoration() {
   let type = useNavigationType()
-  useLayoutEffect(() => {
-    if (type === "PUSH" || type === "REPLACE") {
-      window.scrollTo(0, 0)
-    }
-  }, [type])
+  if (type === "PUSH" || type === "REPLACE") {
+    window.scrollTo(0, 0)
+  }
 }
