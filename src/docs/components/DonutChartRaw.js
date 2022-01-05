ef0be2d6758423f6a0951f905b11139e85afc714
Elijah Meeks <elijahmeeks@gmail.com>
2022-01-04 22:38:14 -0800

    Example of using labelFormatter

--- src/docs/components/DonutChartRaw.js
+++ src/docs/components/DonutChartRaw.js
@@ -9,7 +9,7 @@ const data3 = [
   { x: 1, y: 115698 }
 ]
 
-export default state => {
+export default (state) => {
   const donutSettings = {
     size: [600, 400],
     data: state.changeData ? data2 : data,
@@ -20,8 +20,21 @@ export default state => {
       strokeWidth: 1
     }),
     type: { type: "bar", innerRadius: +state.innerRadius },
-    oLabel: true,
-    margin: { left: 20, top: 20, bottom: 20, right: 20 },
+    oLabel: {
+      orient: "stem",
+      labelFormatter: (d) => (
+        <>
+          <tspan>Hey</tspan>
+          <tspan x="0" y="16">
+            Hey
+          </tspan>
+          <tspan x="0" y="32">
+            {d}
+          </tspan>
+        </>
+      )
+    },
+    margin: { left: 100, top: 100, bottom: 100, right: 100 },
     tooltipContent: "pie",
     hoverAnnotation: true
   }
@@ -33,7 +46,7 @@ export default state => {
   if (state.kind !== "pie") {
     donutSettings.axes = {
       orient: "left",
-      tickFormat: d => +(d * 10) / 10,
+      tickFormat: (d) => +(d * 10) / 10,
       label: { name: "Radial Label", locationDistance: 15 }
     }
   } else {
