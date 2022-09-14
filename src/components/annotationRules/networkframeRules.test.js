b9f033859df28c290e809903e2aebdc5c7a106ec
Eric Laursen <ericjlaursen@gmail.com>
2022-09-14 11:22:28 -0400

    more tests

--- src/components/annotationRules/networkframeRules.test.js
+++ src/components/annotationRules/networkframeRules.test.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { mount } from "enzyme"
 import {
+  svgEncloseRule,
   svgHighlightRule,
+  svgHullEncloseRule,
   svgNodeRule,
   svgReactAnnotationRule,
+  svgRectEncloseRule,
   htmlFrameHoverRule
 } from "./networkframeRules"
 describe("networkframeRules", () => {
@@ -176,20 +179,20 @@ describe("networkframeRules", () => {
         expect(degreeParagraph.length).toBe(0)
       })
 
-      describe('with d.type as "frame-hover" and tooltipContent fn param', () => { 
-        it("without 'optimizeCustomTooltipPosition', calls 'tooltipContent' fn", () => { 
-           const HoverRuleRes = htmlFrameHoverRule({
-             d: {
-               x: 20,
-               y: 20,
-               id: 12,
-               degree: 23,
-               type: 'frame-hover'
-             },
-             nodes: [],
-             nodeIDAccessor: () => 14,
-             tooltipContent: () => <span id="mock-tooltip-content"/>
-           })
+      describe('with d.type as "frame-hover" and tooltipContent fn param', () => {
+        it("without 'optimizeCustomTooltipPosition', calls 'tooltipContent' fn", () => {
+          const HoverRuleRes = htmlFrameHoverRule({
+            d: {
+              x: 20,
+              y: 20,
+              id: 12,
+              degree: 23,
+              type: "frame-hover"
+            },
+            nodes: [],
+            nodeIDAccessor: () => 14,
+            tooltipContent: () => <span id="mock-tooltip-content" />
+          })
           const mounted = mount(<svg>{HoverRuleRes}</svg>)
           const resultOfParamFn = mounted.find("span#mock-tooltip-content")
           expect(mounted.length).toBe(1)
@@ -198,7 +201,7 @@ describe("networkframeRules", () => {
       })
     })
 
-    describe("without d.x or d.y params populated", () => { 
+    describe("without d.x or d.y params populated", () => {
       it('AND d.edge populated, leverages "nodeIDAccessor" successfully', () => {
         const mockD = {
           id: 12,
@@ -222,7 +225,7 @@ describe("networkframeRules", () => {
         expect(mounted.length).toBe(1)
       })
 
-      it('and without d.edge populated, leverages nodes array successfully', () => {
+      it("and without d.edge populated, leverages nodes array successfully", () => {
         const mockD = {
           id: 12,
           degree: 23,
@@ -247,6 +250,197 @@ describe("networkframeRules", () => {
         })
         const mounted = mount(<svg>{HoverRuleRes}</svg>)
         expect(mounted.length).toBe(1)
+      })
+    })
+  })
+
+  describe("svgEncloseRule", () => {
+    const returnVal = (d) => d
+    const returnId = d => d.id
+    it("returns null when no projected nods match d.ids", () => {
+      const res = svgEncloseRule({
+        d: {
+          ids: [2, 3, 4]
+        },
+        projectedNodes: [5, 6, 7],
+        nodeIDAccessor: returnVal
+      })
+      expect(res).toBe(null)
+    })
+    describe("renders", () => {
+      const projectedNodes = [
+        {
+          x: 10,
+          y: 10,
+          id: 2
+        },
+        {
+          x: 20,
+          y: 20,
+          id: 3
+        },        
+        {
+          x: 30,
+          y: 30,
+          id: 4
+        }
+      ]
+
+      const ThisElement = svgEncloseRule({
+        projectedNodes,
+        d: {
+          ids: [2, 3, 4]
+        },
+        nodeIDAccessor: returnId,
+        nodeSizeAccessor: returnId
+      })
+
+      const mounted = mount(<svg>{ThisElement}</svg>)
+
+      it("returns an Annotation", () => { 
+        expect(mounted.find('SemioticAnnotation').length).toBe(1)
+      })
+    })
+  })
+
+  describe("svgRectEncloseRule", () => { 
+    const returnVal = (d) => d
+    const returnId = (d) => d.id
+
+    it("returns null when no projected nods match d.ids", () => {
+      const res = svgRectEncloseRule({
+        d: {
+          ids: [2, 3, 4]
+        },
+        projectedNodes: [5, 6, 7],
+        nodeIDAccessor: returnVal
+      })
+      expect(res).toBe(null)
+    })
+
+    describe("renders", () => {
+      const projectedNodes = [
+        {
+          x: 10,
+          y: 10,
+          id: 2
+        },
+        {
+          x: 20,
+          y: 20,
+          id: 3
+        },        
+        {
+          x: 30,
+          y: 30,
+          id: 4
+        }
+      ]
+
+      it("returns an Annotation", () => {
+        const ThisElement = svgRectEncloseRule({
+          projectedNodes,
+          d: {
+            ids: [2, 3, 4]
+          },
+          nodeIDAccessor: returnId,
+          nodeSizeAccessor: returnId
+        })
+
+        const mounted = mount(<svg>{ThisElement}</svg>)
+        expect(mounted.find("SemioticAnnotation").length).toBe(1)
+      })
+
+      it("adjusts whe  selectedNodes have 'shapeNode' prop", () => {
+        const ThisElement = svgRectEncloseRule({
+          projectedNodes: projectedNodes.map((d, idx) => { 
+            d.shapeNode = true
+            d.x0 = d.x + idx
+            d.x1 = d.x + idx
+            d.y0 = d.y + idx
+            d.y1 = d.y + idx
+            return d
+          }),
+          d: {
+            ids: [2, 3, 4]
+          },
+          nodeIDAccessor: returnId,
+          nodeSizeAccessor: returnId
+        })
+
+        const mounted = mount(<svg>{ThisElement}</svg>)
+        expect(mounted.find("SemioticAnnotation").length).toBe(1)
+      })
+    })
+  })
+
+  describe("svgHullEncloseRule", () => {
+    const returnVal = (d) => d
+    const returnId = (d) => d.id
+
+    it("returns null when no projected nods match d.ids", () => {
+      const res = svgHullEncloseRule({
+        d: {
+          ids: [2, 3, 4]
+        },
+        projectedNodes: [5, 6, 7],
+        nodeIDAccessor: returnVal
+      })
+      expect(res).toBe(null)
+    })
+
+    describe("renders", () => {
+      const projectedNodes = [
+        {
+          x: 10,
+          y: 10,
+          id: 2
+        },
+        {
+          x: 20,
+          y: 20,
+          id: 3
+        },        
+        {
+          x: 30,
+          y: 30,
+          id: 4
+        }
+      ]
+
+      it("returns an Annotation", () => {
+        const ThisElement = svgHullEncloseRule({
+          projectedNodes,
+          d: {
+            ids: [2, 3, 4]
+          },
+          nodeIDAccessor: returnId,
+          nodeSizeAccessor: returnId
+        })
+
+        const mounted = mount(<svg>{ThisElement}</svg>)
+        expect(mounted.find("SemioticAnnotation").length).toBe(1)
+      })
+
+      it("adjusts whe  selectedNodes have 'shapeNode' prop", () => {
+        const ThisElement = svgHullEncloseRule({
+          projectedNodes: projectedNodes.map((d, idx) => {
+            d.shapeNode = true
+            d.x0 = d.x + idx
+            d.x1 = d.x + idx
+            d.y0 = d.y + idx
+            d.y1 = d.y + idx
+            return d
+          }),
+          d: {
+            ids: [2, 3, 4]
+          },
+          nodeIDAccessor: returnId,
+          nodeSizeAccessor: returnId
+        })
+
+        const mounted = mount(<svg>{ThisElement}</svg>)
+        expect(mounted.find("SemioticAnnotation").length).toBe(1)
       })
     })
   })
